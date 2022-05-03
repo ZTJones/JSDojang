578af3ebf6364875495063fad426333efc9f6dd8
Zachary Jones <zacharyjones@microsoft.com>
2022-05-03 14:25:44 -0700

    Initial commit, prepping for fizzbuzz

---
+++ test.js
@@ -0,0 +1,87 @@
+/*
+    ..................................................
+    :                    ......                      :
+    :                 .:||||||||:.                   :
+    :                /            \                  :
+    :               (   o      o   )                 :
+    :-------@@@@----------:  :----------@@@@---------:
+    :                     `--'                       :
+    :                                                :
+    :                                                :
+    :         K I L R O Y   S A Y S   H I !          :
+    :................................................:
+
+*/
+// yo - says dbristol
+
+
+// I see you David.
+
+// I know!  So maybe you should MOD this discussion! </hint> 😂
+// Let's look into modulos next time, I've got a feeling it's going to come in handy. :)
+// Given an array/list of numbers, replace numbers divisble by 3 with Fizz, replace numbers divisible by 5 with Buzz, and replace numbers divisible by both with FIZZBUZZ
+
+let basicList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let basicList2 = ["Jaws", "Friday the 13th", "KPAX", "Airplane!", "Avatar", "The Fifth Element"];
+// this should be solved as [0, 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FIZZBUZZ];
+
+function fizzBuzz(numList){
+    // Insert cool code here
+    // console.log(numList);
+
+    // loop - iterate, or scan through, all of the elements of input array/list. 
+    for(let i = 0; i <= numList.length; i++){
+        // in here.
+        console.log(numList[i]);
+        
+
+        // here
+    }
+    // conditional - if statement, switch statements
+    if(numList[0] == 0){
+        
+    }
+
+
+    // return FizzBuzzList;
+}
+
+
+fizzBuzz(basicList);
+
+// Recursion is fun!
+function fizzBuzzRec(numList){ // a recursive version
+    
+    
+    // fizzBuzzRec( info, more info);
+}
+
+
+// Hello world testing area
+
+
+//Jeff - Test If/Else Loop
+// for (let i= 0; i<16 ; i++ ){
+
+//     if (condition) 
+//     {
+//         i/3= //A Whole Number
+        
+//         //Then replace number with "Fizz"
+        
+//         if (condition)
+        
+//         i/5= //A Whole Number
+        
+//         //Then replace number with "FIZZBUZZ"
+        
+        
+        
+//     } else 
+//     {
+//         // Return i
+//     }
+    
+// }
+
+

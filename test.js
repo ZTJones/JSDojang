8ce33eabedca4d62ca8db7cd7b2195f389e86548
Zachary Jones <zacharyjones@microsoft.com>
2022-05-04 14:19:11 -0700

    Adding info regarding variables

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 /*
+
     ..................................................
     :                    ......                      :
     :                 .:||||||||:.                   :
@@ -21,8 +22,10 @@
 // Let's look into modulos next time, I've got a feeling it's going to come in handy. :)
 // Given an array/list of numbers, replace numbers divisble by 3 with Fizz, replace numbers divisible by 5 with Buzz, and replace numbers divisible by both with FIZZBUZZ
 
+
+
 let basicList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-let basicList2 = ["Jaws", "Friday the 13th", "KPAX", "Airplane!", "Avatar", "The Fifth Element"];
+let movieList = ["Jaws", "Friday the 13th", "KPAX", "Airplane!", "Avatar", "The Fifth Element"];
 // this should be solved as [0, 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FIZZBUZZ];
 
 function fizzBuzz(numList){
@@ -30,7 +33,7 @@ function fizzBuzz(numList){
     // console.log(numList);
 
     // loop - iterate, or scan through, all of the elements of input array/list. 
-    for(let i = 0; i <= numList.length; i++){
+    for(let i = 0; i < numList.length; i++){
         // in here.
         console.log(numList[i]);
         
@@ -84,4 +87,46 @@ function fizzBuzzRec(numList){ // a recursive version
     
 // }
 
+// Varialbles, conditionals (if, else), Loops! (for, while)
+
+// Next dictionaries, arrays/lists, functions
+
+
+
+
+// After this point, define a variable to hold your name
+// True or false?  Boolean
+let quickBool;
+// let quickBool = true; 
+quickBool = true; 
+
+
+const movementSpeed = 5; // const means constant.  It means that this never changes.
+// String
+let dania = "dania";
+// Integers
+let a = 2;
+let b = 3;
+let c = a + b;
+let Cool = "Zach";
+console.log (Cool);
+
+
+if( Cool == "Zach"){
+    console.log("Zach is the best!");
+} else {
+    console.log(Cool + " is the best! Zach is not though.");
+}
+
+while(Cool == "Zach")
+{
+    console.log("Help, I'm stuck in a loop, and everything is bad.");
+
+    Cool = "Joseph";
+}
+
+for(let i = 0; i < 100; i++)
+{
+    console.log(i);
+}
 

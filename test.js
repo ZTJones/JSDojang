775e558e674d7f1be6eb5209d6f104b9b36d4d76
Zachary Jones <zacharyjones@microsoft.com>
2022-05-05 14:50:43 -0700

    Fun with variables, arrays, and 2D arrays

--- test.js
+++ test.js
@@ -35,7 +35,7 @@ function fizzBuzz(numList){
     // loop - iterate, or scan through, all of the elements of input array/list. 
     for(let i = 0; i < numList.length; i++){
         // in here.
-        console.log(numList[i]);
+        // console.log(numList[i]);
         
 
         // here
@@ -87,7 +87,7 @@ function fizzBuzzRec(numList){ // a recursive version
     
 // }
 
-// Varialbles, conditionals (if, else), Loops! (for, while)
+// Variables, conditionals (if, else), Loops! (for, while)
 
 // Next dictionaries, arrays/lists, functions
 
@@ -105,28 +105,143 @@ const movementSpeed = 5; // const means constant.  It means that this never chan
 // String
 let dania = "dania";
 // Integers
-let a = 2;
-let b = 3;
-let c = a + b;
-let Cool = "Zach";
-console.log (Cool);
+// let a = 2;
+// let b = 3;
+// let c = a + b;
+// let Cool = "Zach";
+// console.log (Cool);
 
 
-if( Cool == "Zach"){
-    console.log("Zach is the best!");
-} else {
-    console.log(Cool + " is the best! Zach is not though.");
+// if( Cool == "Zach"){
+//     console.log("Zach is the best!");
+// } else {
+//     console.log(Cool + " is the best! Zach is not though.");
+// }
+
+// while(Cool == "Zach")
+// {
+//     console.log("Help, I'm stuck in a loop, and everything is bad.");
+
+//     Cool = "Joseph";
+// }
+
+// for(let i = 0; i < 100; i++)
+// {
+//     console.log(i);
+// }
+
+
+
+// Make some variables
+// Age
+// DOB
+// FaveVehicle
+// likesDogs
+
+let age = 27;
+let dob = "19/10/1994";
+let FaveVehicle = "RollsRoyce";
+let likedogs = false;
+
+let dateThing =  Date.now();
+// dateThing.getFullYear()
+
+
+
+let daniaAge = 104;
+let daniaDOB = "01/01/1900";
+let daniaFavVehicle = "rollerblabes";
+let danialikesDogs = true;
+
+// console.log(daniaAge);
+
+daniaAge = 122;
+// console.log(daniaAge);
+
+
+
+// c += 10;
+// console.log(c);
+// c++;
+// console.log(c);
+
+
+// while(c < 1000)
+// {
+//     c += 10;
+//     console.log(c);
+// }
+
+let wordList = ["Hello", "World", 42, "42", "Pizza"];
+
+// console.log(wordList[0]);  // What do we get?
+// console.log(wordList[4]);  // and here?
+// console.log(wordList[2]);
+// console.log(wordList[3]); // What's different about how these are output?
+
+
+let map = [
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    
+]
+
+// console.log(map);
+// console.log(map[0]);
+// console.log(map[0][5]);
+// console.log(map[0][5]);
+// console.log(map[0][5]);
+let HoldingArray = [
+    // [],
+];
+
+for(let y = 0; y < map.length; y++){
+    HoldingArray.push([]);
+    for(let x = 0; x < map[0].length; x++){
+        // console.log(map[y][x]);
+        HoldingArray[y].push(map[y][x]);
+    }
+    // console.log(map[y]);
 }
+HoldingArray[3][5] = 25;
 
-while(Cool == "Zach")
-{
-    console.log("Help, I'm stuck in a loop, and everything is bad.");
+console.log("Holding:", HoldingArray);
+console.log("Map:", map);
 
-    Cool = "Joseph";
-}
 
-for(let i = 0; i < 100; i++)
-{
-    console.log(i);
-}
+
+// let testA = 5;
+    // let testB = testA;
+    // testB = testB + 10;
+    // console.log("test A: " + testA);
+    // console.log("test B: " + testB);
+
+
+
+
+// let x = [0,1,2,3];
+// let y = x;
+// console.log("before change " + x[0]);
+// console.log("before change " + y[0]);
+// y[0] = 1000;
+// console.log("after " +x[0]);
+// console.log("after " + y[0]);
+// y.push(20)
+// console.log("test x:", x);
+// console.log("test y:", y);
+// console.log(y[1]);
+// console.log(y[0]);
+
+
 
